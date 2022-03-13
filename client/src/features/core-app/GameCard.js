65a6bf83811a7d6cb627cc38f24b1d8d841e1c8c
James Bosley <james_bosley@hotmail.co.uk>
2022-03-13 20:49:54 +0000

    GameCard changes.

--- client/src/features/core-app/GameCard.js
+++ client/src/features/core-app/GameCard.js
@@ -1,14 +1,30 @@
 import React from "react";
 
 const GameCard = (props) => {
+  const playerStyle = (gender) => {
+    if (gender === "M") {
+      return { backgroundColor: "lightblue" };
+    } else {
+      return { backgroundColor: "pink" };
+    }
+  };
+
   return (
-    <div>
+    <div className="gameCard">
       <p>This is game {props.data.id}</p>
       {props.data.pairA.map((player) => {
-        return <p key={player.id}>{player.display_name}</p>;
+        return (
+          <p key={player.id} style={playerStyle(player.gender)}>
+            {player.display_name}
+          </p>
+        );
       })}
       {props.data.pairB.map((player) => {
-        return <p key={player.id}>{player.display_name}</p>;
+        return (
+          <p key={player.id} style={playerStyle(player.gender)}>
+            {player.display_name}
+          </p>
+        );
       })}
       {props.enableScore ? <div>SCORES</div> : null}
     </div>
